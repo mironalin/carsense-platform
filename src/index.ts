38d3c8f34590992e73e4de829997671ab81019cb
mironalin <alinmiron3@gmail.com>
2025-03-19 12:53:37 +0200

    Initial commit
    Hono, Drizzle, Neon postgres, Better Auth

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { auth } from "../lib/auth";
+import { authRoute } from "../routes/authRoute";
+
+const app = new Hono().use(logger());
+
+const apiRoutes = app.basePath("/api").route("/auth/**", authRoute);
+
+export default app;
+export type ApiRoutes = typeof apiRoutes;
