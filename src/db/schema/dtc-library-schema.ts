0321bd574ff69d1ba14105d50355a71ccdce66cd
mironalin <alinmiron3@gmail.com>
2025-03-24 19:51:15 +0200

    Add DTC library database schema and models

---
+++ src/db/schema/dtc-library-schema.ts
@@ -0,0 +1,35 @@
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import {
+  createInsertSchema,
+  createSelectSchema,
+  createUpdateSchema,
+} from "drizzle-zod";
+
+export const severityEnum = pgEnum("severity", ["low", "medium", "high"]);
+
+export const DTCLibraryTable = pgTable("dtcLibrary", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  uuid: uuid("uuid").defaultRandom(),
+  code: text("code").notNull().unique(),
+  description: text("description").notNull(),
+  severity: severityEnum("severity").notNull(),
+  affectedSystem: text("affectedSystem"),
+  category: text("category"),
+  createdAt: timestamp("createdAt").notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt")
+    .$onUpdate(() => new Date())
+    .notNull(),
+});
+
+export const insertDTCLibrarySchema = createInsertSchema(DTCLibraryTable);
+
+export const updateDTCLibrarySchema = createUpdateSchema(DTCLibraryTable);
+
+export const selectDTCLibrarySchema = createSelectSchema(DTCLibraryTable);
