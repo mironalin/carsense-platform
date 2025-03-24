5c51b6f264c135ce681606f5d03fc21cbd01615e
mironalin <alinmiron3@gmail.com>
2025-03-24 19:51:20 +0200

    Add location table schema with Drizzle ORM

---
+++ src/db/schema/locations-schema.ts
@@ -0,0 +1,32 @@
+import {
+  doublePrecision,
+  integer,
+  pgTable,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { vehiclesTable } from "./vehicles-schema";
+import {
+  createInsertSchema,
+  createSelectSchema,
+  createUpdateSchema,
+} from "drizzle-zod";
+
+export const locationsTable = pgTable("locations", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  uuid: uuid("uuid").defaultRandom(),
+  vehicleId: integer("vehicle_id").references(() => vehiclesTable.id),
+  latitude: doublePrecision("latitude").notNull(),
+  longitude: doublePrecision("longitude").notNull(),
+  accuracy: doublePrecision("accuracy"),
+  createdAt: timestamp("createdAt").notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt")
+    .$onUpdate(() => new Date())
+    .notNull(),
+});
+
+export const createLocationSchema = createInsertSchema(locationsTable);
+
+export const selectLocationSchema = createSelectSchema(locationsTable);
+
+export const updateLocationSchema = createUpdateSchema(locationsTable);
