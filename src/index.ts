0636ccbd9e32b295f0f0080d288277cb0cf8e17f
mironalin <alinmiron3@gmail.com>
2025-03-25 22:45:03 +0200

    Refactor application structure by separating app initialization into a new module

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,7 @@
-import configureOpenAPI from "@/lib/configure-open-api";
-import { createApp } from "@/lib/create-app";
-import { authRoute } from "@/routes/auth-route";
-import { testRoute } from "@/routes/test-route";
-import { vehiclesRoute } from "@/routes/vehicles-route";
+import env from "../env";
+import app from "./app";
 
-// try {
-//   const result = await auth.api.signInEmail({
-//     body: {
-//       email: "test@example.com",
-//       password: "password",
-//     },
-//     asResponse: true,
-//   });
-
-//   console.log(result.headers.get("set-auth-token"));
-// } catch (error) {
-//   if (error instanceof APIError) {
-//     console.log(error.message, error.status);
-//   }
-// }
-
-const app = createApp();
-
-configureOpenAPI(app);
-
-export const apiRoutes = app
-  .basePath("/api")
-  .route("/auth/**", authRoute)
-  .route("/test", testRoute)
-  .route("/vehicles", vehiclesRoute);
-
-export default app;
-export type ApiRoutes = typeof apiRoutes;
+export default {
+  port: env.PORT,
+  fetch: app.fetch,
+};
