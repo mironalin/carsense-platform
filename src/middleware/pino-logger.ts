f8ca71cdfda890f33f1a9cb9f714c206c70a3979
mironalin <alinmiron3@gmail.com>
2025-03-27 00:43:32 +0200

    Refactor Pino logger middleware to use updated logger instance from Hono-Pino for improved logging configuration

--- src/middleware/pino-logger.ts
+++ src/middleware/pino-logger.ts
@@ -1,13 +1,15 @@
-import { pinoLogger as logger } from "hono-pino";
+import { pinoLogger as honoLogger } from "hono-pino";
 import pino from "pino";
 import pretty from "pino-pretty";
 
 import env from "../../env";
 
 export function pinoLogger() {
-  return logger({
-    pino: pino({
-      level: env.LOG_LEVEL || "info",
-    }, env.NODE_ENV === "production" ? undefined : pretty()),
+  const logger = pino({
+    level: env.LOG_LEVEL || "info",
+  }, env.NODE_ENV === "production" ? undefined : pretty());
+
+  return honoLogger({
+    pino: logger,
   });
 }
