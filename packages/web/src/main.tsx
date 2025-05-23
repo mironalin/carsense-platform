1e8627a777b29de4eaf37c3e296a8b3ebdffcf52
mironalin <alinmiron3@gmail.com>
2025-05-23 17:20:04 +0300

    Configure antfu's eslint config globally

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,11 +1,12 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
 
-import './index.css'
+import ReactDOM from 'react-dom/client'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
+
+import './index.css'
 
 // Create a new router instance
 const router = createRouter({ routeTree })
