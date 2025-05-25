74df78bb2be92fa91556b4df7691ecb963fbe713
mironalin <alinmiron3@gmail.com>
2025-05-25 15:50:09 +0300

    Integrate LoaderPage and NotFoundPage into root routing configuration
    - Added LoaderPage as a pending component to enhance user experience during data loading.
    - Reintroduced NotFoundPage as the not found component for improved navigation when encountering non-existent routes.
    - Enabled suspense wrapping for better handling of loading states.

--- packages/web/src/routes/__root.tsx
+++ packages/web/src/routes/__root.tsx
@@ -1,15 +1,20 @@
-import { NotFoundPage } from "@/components/not-found-page";
 import type { QueryClient } from "@tanstack/react-query";
 
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+
+import { LoaderPage } from "@/components/loader-page";
+import { NotFoundPage } from "@/components/not-found-page";
 
 type RouterContext = {
   queryClient: QueryClient;
 };
 
 export const Route = createRootRouteWithContext<RouterContext>()({
-  notFoundComponent: () => <NotFoundPage />,
   errorComponent: () => <div>Error</div>,
+  notFoundComponent: () => <NotFoundPage />,
+  wrapInSuspense: true,
+  pendingComponent: () => <LoaderPage />,
+  // loader: () => void 0,
   component: () => (
     <>
       <Outlet />
